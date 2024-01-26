5376fb8573e67c7f99b68c97277084d634051879
ttakahisa <t.takahisa@gmail.com>
2024-01-26 15:16:52 +0000

    Spring Boot3 init

---
+++ src/main/java/takahisa/study/springboot3sample/SpringBoot3SampleApplication.java
@@ -0,0 +1,13 @@
+package takahisa.study.springboot3sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3SampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takahisa/study/springboot3sample/SpringBoot3SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package takahisa.study.springboot3sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
